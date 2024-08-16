c93a16f91b9b860118f3c33e4681a975585b1e39
methodius pkemoi <pkemoimethodius@gmail.com>
2024-08-16 17:45:16 +0300

    Appropriate changes

---
+++ src/components/ArticleList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}){
+  return(
+    <main>
+        {posts.map((post) =>(
+            <Article key={post.id} date={post.date} title={post.title} preview= {post.preview} minutes= {post.minutes}/>
+        ))}
+    </main>
+  )
+}
+
+export default ArticleList;
